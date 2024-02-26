2ea619ad293d9969b8cbba34faaf64984cb5491a
Prince Patel <prpatel0949@gmail.com>
2024-02-27 01:26:37 +0530

    remove project

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverage: true,
-  resetMocks: true,
-  clearMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'index.ts', 'vite-env.d.ts'],
-}
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Color } from './color'
-export { default as Colors } from './colors'
-export { default as Pallete } from './pallete'
